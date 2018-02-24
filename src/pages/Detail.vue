b4ce7ff5233f95c13d89ff50962bfaee3ee1de64
leon_yu <494076545@qq.com>
2018-02-24 10:24:32 +0800

    text

---
+++ src/pages/Detail.vue
@@ -0,0 +1,35 @@
+<!-- Detail.vue -->
+<template>
+  <div class="detail">
+    <detail-header></detail-header>
+    <img src="../assets/img/monsterHunter.jpg" alt="">
+    <p>《怪物猎人：世界》是由CAPCOM制作发行的一款动作冒险游戏，在全球范围内都享有盛赞，本代怪物猎人发生了诸多进化，玩法与画面都有非常巨大的进步，游戏最多支持4人联机。在游戏中玩家将扮演一位猎人，需要探索一片新发现的神秘土地，关于这片土地人们知之甚少，所以将这片大陆称为“新世界”。猎人们必须利用他们的聪明才智和个人能力才能在激烈、变化多端的战斗中存活下来，并且最终成为终极猎人。</p>
+    <p></p>
+  </div>
+</template>
+<style>
+  .detail {
+    padding: 0.24rem;
+    font-size: 12px;
+  }
+  img {
+    display: block;
+    width: 80%;
+    margin: 0 auto 0.2rem;
+  }
+  p {
+    font-size: 14px;
+    line-height: 0.5rem;
+    text-align: justify;
+    padding-bottom: 0.24rem;
+  }
+</style>
+<script>
+  import DetailHeader from '../components/DetailHeader'
+
+  export default {
+    components: {
+      DetailHeader
+    }
+  }
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,50 @@
+<!-- Home.vue -->
+<template>
+  <div class="container">
+    <!-- .vue 不区分大小写，大写字母改成-连接 -->
+    <home-header></home-header>
+
+    <div class="content">
+      <ul class="cont_ul">
+        <list
+        v-for="item in items"
+        :price="item.price"
+        :title="item.title">
+
+        </list>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+  //导入用到的子组件
+  import HomeHeader from '../components/HomeHeader'
+  import List from '../components/List'
+
+  export default{
+      data(){
+          return{
+              items:[
+                { price:"129.00",title:"怪物猎人世界" },
+                { price:"256.00",title:"怪物猎人世界" },
+                { price:"399.00",title:"怪物猎人世界" },
+                { price:"998.00",title:"怪物猎人世界" },
+                { price:"99.00",title:"怪物猎人世界" },
+                { price:"89.00",title:"怪物猎人世界" },
+                { price:"188.00",title:"怪物猎人世界" },
+                { price:"999.98",title:"怪物猎人世界" },
+              ]
+          }
+      },
+    //在components 字段中，包含导入的子组件
+    components:{
+          HomeHeader,
+          List
+    }
+  }
+</script>
